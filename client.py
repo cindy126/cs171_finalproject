c108714215ca16dad098ed37505291c439f7cb24
Cindy Zhao <cindyzhao126@gmail.com>
2021-02-28 17:43:51 -0800

    maybe done with demo

--- client.py
+++ client.py
@@ -6,65 +6,64 @@ import time
 import os
 
 PORTS = {
-    '1': 3000,
-    '2': 3001,
-    '3': 3002,
-    '4': 3003,
-    '5': 3004
+    '1': 5000,
+    '2': 5001,
+    '3': 5002,
+    '4': 5003,
+    '5': 5004
 }
 
-if __name__ == "__main__":
-    processID = sys.argv[1]
-    server_message = "C" + processID
+processID = sys.argv[1]
+server_message = "C" + processID
 
-    #connect to servers
-    client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket1.connect((socket.gethostname(), PORTS["1"]))
-    client_socket1.send(server_message.encode())
-    msg = client_socket1.recv(1024)
-    print(msg.decode())
+#connect to servers
+client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket1.connect((socket.gethostname(), PORTS["1"]))
+client_socket1.send(server_message.encode())
+msg = client_socket1.recv(1024)
+print(msg.decode())
 
-    client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket2.connect((socket.gethostname(), PORTS["2"]))
-    client_socket2.send(server_message.encode())
-    msg = client_socket2.recv(1024)
-    print(msg.decode())
+client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket2.connect((socket.gethostname(), PORTS["2"]))
+client_socket2.send(server_message.encode())
+msg = client_socket2.recv(1024)
+print(msg.decode())
 
-    client_socket3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket3.connect((socket.gethostname(), PORTS["3"]))
-    client_socket3.send(server_message.encode())
-    msg = client_socket3.recv(1024)
-    print(msg.decode())
+client_socket3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket3.connect((socket.gethostname(), PORTS["3"]))
+client_socket3.send(server_message.encode())
+msg = client_socket3.recv(1024)
+print(msg.decode())
 
-    client_socket4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket4.connect((socket.gethostname(), PORTS["4"]))
-    client_socket4.send(server_message.encode())
-    msg = client_socket4.recv(1024)
-    print(msg.decode())
+client_socket4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket4.connect((socket.gethostname(), PORTS["4"]))
+client_socket4.send(server_message.encode())
+msg = client_socket4.recv(1024)
+print(msg.decode())
 
-    client_socket5 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket5.connect((socket.gethostname(), PORTS["5"]))
-    client_socket5.send(server_message.encode())
-    msg = client_socket5.recv(1024)
-    print(msg.decode())
-
-    while True:
-        command = input("Which server? (1-5 or all): ")
-        msg = input("What is the message?: ")
-        msg = msg + " -C" + processID
-        if command == '1':
-            client_socket1.send(msg.encode())
-        elif command == '2':
-            client_socket2.send(msg.encode())
-        elif command == '3':
-            client_socket3.send(msg.encode())
-        elif command == '4':
-            client_socket4.send(msg.encode())
-        elif command == '5':
-            client_socket5.send(msg.encode())
-        elif command == 'all':
-            client_socket1.send(msg.encode())
-            client_socket2.send(msg.encode())
-            client_socket3.send(msg.encode())
-            client_socket4.send(msg.encode())
-            client_socket5.send(msg.encode())
+client_socket5 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket5.connect((socket.gethostname(), PORTS["5"]))
+client_socket5.send(server_message.encode())
+msg = client_socket5.recv(1024)
+print(msg.decode())
+        
+while True:
+    command = input("Which server? (1-5 or all): ")
+    msg = input("What is the message?: ")
+    msg = msg + " -C" + processID
+    if command == '1':
+        client_socket1.send(msg.encode())
+    elif command == '2':
+        client_socket2.send(msg.encode())
+    elif command == '3':
+        client_socket3.send(msg.encode())
+    elif command == '4':
+        client_socket4.send(msg.encode())
+    elif command == '5':
+        client_socket5.send(msg.encode())
+    elif command == 'all':
+        client_socket1.send(msg.encode())
+        client_socket2.send(msg.encode())
+        client_socket3.send(msg.encode())
+        client_socket4.send(msg.encode())
+        client_socket5.send(msg.encode())
